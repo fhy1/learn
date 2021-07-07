8a20e237cd66b79ff6d6c6d39d1b053279239fa0
fenghaoyu <fenghaoyu@woouo.com>
2021-07-07 17:44:59 +0800

    watcher 实现

--- vue/my-study/my-vue/myvue.js
+++ vue/my-study/my-vue/myvue.js
@@ -4,7 +4,7 @@ function defineReactive (obj, key, val) {
 
   Object.defineProperty(obj, key, {
     get () {
-      console.log('get', key)
+      // console.log('get', key)
       return val
     },
     set (v) {
@@ -12,7 +12,7 @@ function defineReactive (obj, key, val) {
         // 如果传入的是一个对象仍需要做响应式处理
         val = v
         observe(v)
-        console.log('set', key)
+        // console.log('set', key)
       }
     }
   })
@@ -54,6 +54,9 @@ class MyVue {
     proxy(this)
 
     // 3、编译
+
+    const com = new Compile(options.el, this)
+    console.log(com)
   }
 
   set (obj, key, val) {
@@ -61,4 +64,91 @@ class MyVue {
   }
 }
 
+class Compile {
+  constructor (el, vm) {
+    // 保存KVue 实例
+    this.$vm = vm
+    // 编译模板树
+    this.compile(document.querySelector(el))
+  }
+
+  // el 模板根节点
+  compile (el) {
+    // 遍历el
+    // 获取el所有子节点
+    el.childNodes.forEach(node => {
+      // 2.判断node类型
+      if (node.nodeType === 1) {
+        // 元素
+        console.log('element', node.nodeName)
+
+        this.compileElement(node)
+        if (node.childNodes.length > 0) {
+          this.compile(node)
+        }
+        // 递归
+      } else if (this.isInter(node)) {
+        // 插值文本
+        console.log('text', node.textContent)
+        this.compileText(node)
+      }
+    })
+  }
+
+  // 处理插值文本 {{xx}}
+  compileText (node) {
+    this.update(node, RegExp.$1, 'text')
+    // node.textContent = this.$vm[RegExp.$1]
+  }
+
+  update (node, exp, dir) {
+    const fn = this[dir + 'Updater']
+    fn && fn(node, this.$vm[exp])
+  }
+
+  textUpdater (node, val) {
+    node.textContent = val
+  }
+
+  // 编译element
+  compileElement (node) {
+    // 1 获取当前元素的素有属性，并判断他们是不是动态的
+    const nodeAttrs = node.attributes
+    Array.from(nodeAttrs).forEach(attr => {
+      const attrName = attr.name
+      const exp = attr.value
+      // 判断attrName是否是指令或事件等动态
+      if (attrName.startsWith('k-')) {
+        const dir = attrName.substring(2)
+        this[dir] && this[dir](node, exp)
+      }
+    })
+  }
+
+  // k-text
+  text (node, exp) {
+    node.textContent = this.$vm[exp]
+  }
+
+  isInter (node) {
+    return node.nodeType === 3 && /\{\{(.*)\}\}/.test(node.textContent)
+  }
+}
+
 console.log(MyVue)
+
+const watchers = []
+// 负责具体更新任务的 Watcher
+class Watcher {
+  constructor (vm, key, updaterFn) {
+    this.vm = vm
+    this.key = key
+    this.updaterFn = updaterFn
+
+    watchers.push(this)
+  }
+
+  update () {
+    this.updaterFn.call(this.vm, this.vm[this.key])
+  }
+}
